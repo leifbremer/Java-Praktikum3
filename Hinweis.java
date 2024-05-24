8f8655f4a667467ead279aaf82aab610c450af1e
Nicolas Lyer <lyer.nicolas @gmail.com>
2024-05-24 23:13:14 +0200

    Methodenkommentare für Hinweis hinzugefügt

--- Hinweis.java
+++ Hinweis.java
@@ -1,21 +1,35 @@
-
 /**
- * Beschreiben Sie hier die Klasse Information.
+ * Klasse Hinweis
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * Repräsentativ für einen Gegenstand, der einen Hinweis an den User enthält.
+ * Erbt von der Klasse Gegenstand.
+ * 
+ * @author (Nicolas Lyer, Leif Bremer) 
+ * @version (Version 1, 24.05.2024)
  */
 public class Hinweis extends Gegenstand
 {
     private String hinweis;
 
+    /**
+     * Hinweis Konstruktor
+     *
+     * Initialiesurung des Hinweisetextes.
+     *
+     * @param hinweis Der Hinweistext.
+     */
     public Hinweis(String hinweis){
         this.hinweis = hinweis;
     }
 
+    /**
+     * Methode benutzen
+     *
+     * Gibt den Hinweistext zurück.
+     *
+     * @return Der Hinweistext.
+     */
     public String benutzen(){
         return hinweis;
     }
-    
-    
 }
