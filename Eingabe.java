21ea5c22a106bb40c8935e260c223b590ce3951a
Nicolas Lyer <lyer.nicolas @gmail.com>
2024-05-24 23:31:41 +0200

    Methodenkommentare für Eingabe hinzugefügt

--- Eingabe.java
+++ Eingabe.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 import java.util.HashMap;
 
 /**
- * Beschreiben Sie hier die Klasse Eingabe.
+ * Klasse Eingabe
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * Repräsentativ für einen Gegenstand, der eine Benutzereingabe durchführt.
+ * 
+ * @author (Nicolas Lyer, Leif Bremer) 
+ * @version (Version 1, 24.05.2024)
  */
 public class Eingabe extends Gegenstand
 {
@@ -13,16 +15,42 @@ public class Eingabe extends Gegenstand
     private Raum raumVon;
     private String abfrage;
     
+    /**
+     * Eingabe Konstruktor
+     * 
+     * Initialisiert Schlüsse, Ausgangsraum und Abfrage.
+     *
+     * @param schluessel Der Schlüssel, der für die erfolgreiche Eingabe erforderlich ist.
+     * @param raumVon Der Raum, aus dem die Eingabe erfolgt.
+     * @param abfrage Die Abfrage, die dem Benutzer gestellt wird.
+     */
     public Eingabe(String schluessel, Raum raumVon, String abfrage){
         this.schluessel = schluessel;
         this.raumVon = raumVon;
         this.abfrage = abfrage; 
     }
     
+    /**
+     * Methode benutzen
+     *
+     * Benutzung des Eingabegegenstandes.
+     * Ausgabe, dass eine Eingabe möglich ist.
+     *
+     * @return Eine Nachricht, die anzeigt, dass eine Eingabe möglich ist.
+     */
     public String benutzen(){
         return "Sie können hier etwas eingeben";
     }
     
+    /**
+     * Methode eingeben
+     *
+     * Methode für die Benutzereingabe.
+     * Führt die Benutzereingabe durch und überprüft,
+     * ob sie mit dem richtigen Schlüssel entspricht.
+     *
+     * @return 'true' bei richtigem Schlüssel, sonst 'false'.
+     */
     public boolean eingeben(){
         Scanner leser = new Scanner(System.in);
         System.out.println(abfrage);
@@ -30,10 +58,25 @@ public class Eingabe extends Gegenstand
         return vergleicheSchluessel(leser.nextLine());
     }
 
+    /**
+     * Methode getWo
+     * 
+     * Gibt den Raum, in der die Eingabe stattfand zurück.
+     *
+     * @return Der Raum, in der die Eingabe stattfand zurück.
+     */
     public Raum getWo(){
         return raumVon;
     }
     
+    /**
+     * Methode vergleicheSchluessel
+     *
+     * Vergleicht die Benutzereingabe mit dem erwarteten Schlüssel.
+     *
+     * @param schluessel Der vom User eingegebene Schlüssel
+     * @return 'true' bei richtigem Schlüssel, sonst 'false'.
+     */
     private boolean vergleicheSchluessel(String schluessel){
         if(this.schluessel.equals(schluessel)){
             return true;
