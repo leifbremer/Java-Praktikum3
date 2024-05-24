b4fbec0e76ec727f90fb5cffa1344445af8b4734
Nicolas Lyer <lyer.nicolas @gmail.com>
2024-05-24 23:08:01 +0200

    Methodenkommentare für Kaputt hinzugefügt

--- Kaputt.java
+++ Kaputt.java
@@ -1,21 +1,38 @@
-
 /**
- * Beschreiben Sie hier die Klasse Kaputt.
+ * Klasse Kaputt
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * Die Klasse ist repräsentativ für einen kaputten Gegenstand, der repariert werden muss.
+ * Erbt von der Klasse Gegenstand
+ * 
+ * @author (Nicolas Lyer, Bremer) 
+ * @version (Version 1, 24.05.2024)
  */
 public class Kaputt extends Gegenstand
 {
     boolean kaputt;
     private Gegenstand werkzeug;
     
+    /**
+     * Kaputt Konstruktor
+     *
+     * Initialisiert das Objekt als kaputt,
+     * setzt das benötigte Werkzeug.
+     *
+     * @param werkzeug Das Werkzeug, das zur Reparatur des Gegenstands benötigt wird
+     */
     public Kaputt(Gegenstand werkzeug)
     {
         kaputt = true;
         this.werkzeug = werkzeug;
     }
     
+    /**
+     * Methode benutzen
+     *
+     * Methode zum Benutzen des kaputten Gegenstandes.
+     *
+     * @return Ausgabe, ob der Gegenstand repariert werden muss oder nicht. 
+     */
     public String benutzen()
     {
         if (kaputt){
@@ -25,10 +42,22 @@ public class Kaputt extends Gegenstand
         }
     }
     
+    /**
+     * Methode gibWerkzeug
+     *
+     * Die Methode gibt das Werkzeug zurück, das zur Reparatur des Gegenstands benötigt wird.
+     *
+     * @return Das Werkzeug, das zur Reparatur des Gegenstands benötigt wird.
+     */
     public Gegenstand gibWerkzeug(){
         return werkzeug;
     }
     
+    /**
+     * Methode reparieren
+     *
+     * Repariert den Gegenstand, indem kaputt auf 'false' gesetzt wird.
+     */
     public void reparieren()
     {
         kaputt = false;
